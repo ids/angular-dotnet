92e5af8c7a4b601443901c5f0f82817e83e0a679
Sean Hignett <sean@idstudios.io>
2022-09-25 09:44:55 -0600

    trying on that new program styling again

--- Program.cs
+++ Program.cs
@@ -3,76 +3,65 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
    
-namespace AngularDotNet
+static void CreateDbIfNotExists(IHost host)
 {
-    public class Program
+    using (var scope = host.Services.CreateScope())
     {
-        private static void CreateDbIfNotExists(IHost host)
+        var services = scope.ServiceProvider;
+        try
         {
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<AngularDotNetContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    Log.Error(ex, "An error occurred creating the DB.");
-                }
-            }
+            var context = services.GetRequiredService<AngularDotNetContext>();
+            DbInitializer.Initialize(context);
         }
-
-        public static void Main(string[] args)
+        catch (Exception ex)
         {
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.Console()
-                .CreateLogger();
-
-            Log.Information("Starting up the Host!");
-
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            //builder.Services.AddControllersWithViews();
-            builder.Services.AddSingleton<DapperContext>(); 
-            builder.Services.AddScoped<ICompanyRepository, CompanyRepository>(); 
-            builder.Services.AddControllers();
-            builder.Services.AddDbContext<AngularDotNetContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("EFConnection")));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            } 
-            else {
-                CreateDbIfNotExists(app);
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller}/{action=Index}/{id?}");
-
-            app.MapFallbackToFile("index.html"); ;
-
-            app.Run();
+            Log.Error(ex, "An error occurred creating the DB.");
         }
-
     }
 }
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Log.Information("Starting up the Host!");
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<DapperContext>(); 
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>(); 
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AngularDotNetContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EFConnection")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+} 
+else {
+    CreateDbIfNotExists(app);
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();
 
 
